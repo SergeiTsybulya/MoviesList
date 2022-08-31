fee738c8efdf370cb0460d3c05769a5b32df3677
SergeiTsybulya <sergtsybulya@gmail.com>
2022-08-31 18:27:17 +0300

    Initial Commit

---
+++ MovieList/Model/DescriptionModel.swift
@@ -0,0 +1,14 @@
+//
+//  DescriptionModel.swift
+//  MovieList
+//
+//  Created by Sergei Tsybulya on 31.08.2022.
+//  Copyright © 2022 STDevelopment. All rights reserved.
+//
+
+import UIKit
+
+struct Description {
+    let title: String
+    let year: Int
+}
