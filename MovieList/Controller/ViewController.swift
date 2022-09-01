7802d718a2b790332f38f6d872601adb6d843824
SergeiTsybulya <sergtsybulya@gmail.com>
2022-09-01 11:45:46 +0300

    1. Fix: Replace movies Array in to movies OrederedSet

--- MovieList/Controller/ViewController.swift
+++ MovieList/Controller/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import OrderedCollections
 
 class ViewController: UIViewController {
     
@@ -13,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak private var yearTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
-    var movies = [Description]()
+    var movies = OrderedSet<Description>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +26,11 @@ class ViewController: UIViewController {
     @IBAction func addPressed(_ sender: UIButton) {
         guard let text = titleTextField.text, !text.isEmpty, let year = yearTextField.text, !year.isEmpty else { return }
         let newMovie = Description(title: text, year: Int(year) ?? 0)
-        if movies.contains(newMovie) {
-            let alert = UIAlertController(title: "Oops!", message: "The movie is allready in list", preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        } else {
             DispatchQueue.main.async {
                 self.movies.append(newMovie)
                 self.tableView.reloadData()
             }
-        }
+        
     }
 }
 
--- MovieList/Model/DescriptionModel.swift
+++ MovieList/Model/DescriptionModel.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-struct Description: Equatable {
+//protocol hashDescription {
+//    var title: String { get }
+//    var year: Int { get }
+//}
+
+struct Description: Hashable, Equatable {
     let title: String
     let year: Int
 }
